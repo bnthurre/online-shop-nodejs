d854f99aebf1720c9b02363403bd460cff1f7744
bushrahurre959@gmail.com <marriamhurre@gmail.com>
2024-02-06 15:03:28 +0300

    adding pug shop templete

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set('views', "views");
+
+
 const adminData = require("./router/admin");
 const shopRoute = require("./router/shop");
 
-const path = require("path");
 
-
-const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(shopRoute);
 app.use("/admin", adminData.routes);
--- router/shop.js
+++ router/shop.js
@@ -6,7 +6,7 @@ const adminData = require('./admin')
 const router = express.Router();
 
 router.get('/',(req, res,next)=>{
-    console.log(adminData.products);
-    res.sendFile(path.join(rootDir,'views', 'shop.html'));
+    const products =adminData.products;
+    res.render('shop', {prod:products, docTitle: "Shop"} )
 })
 module.exports =router;
