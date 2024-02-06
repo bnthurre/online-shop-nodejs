db75e690ea969a15f18301c17b74208dafc8ec3d
bushrahurre959@gmail.com <marriamhurre@gmail.com>
2024-02-06 13:26:10 +0300

    serving files statically

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const adminRoute = require("./router/admin");
+const shopRoute = require("./router/shop");
 
-const express =require('express');
-const bodyParser =require('body-parser');
-const adminRoute= require('./router/admin');
-const shopRoute = require('./router/shop')
+const path = require("path");
 
-const path = require('path');
 
 const app = express();
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(shopRoute);
-app.use('/admin',adminRoute);
+app.use("/admin", adminRoute);
 
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,  'views','404.html'));
-})
-
-
-
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(9000);
